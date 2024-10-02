29c9bf9f65de3811a062aa02bba923c74e73daa8
otienojoseph <otienoj.cruz@gmail.com>
2024-10-02 05:48:49 +0300

    fix: Use Array.push

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
-  for (const [idx, val] of newArray.entries()) {
-    newArray[idx] = appendString + val;
+  const newArray = [];
+  for (const val of array) {
+    newArray.push(appendString + val);
   }
 
   return newArray;
