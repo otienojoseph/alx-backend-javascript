481322a2e9c60e630a49bff9c892a983dea8354e
otienojoseph <otienoj.cruz@gmail.com>
2024-10-02 20:59:11 +0300

    styles: Use spaces instead of tabs

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,7 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Resolved");
-        }, 1000);
-
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Resolved');
+    }, 1000);
+  });
 }
