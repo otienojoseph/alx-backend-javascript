baf07cfaa018424305ac40bf67c67eb1d62632b8
otienojoseph <otienoj.cruz@gmail.com>
2024-10-03 04:57:51 +0300

    fix: Add correct promise format

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,16 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      if (results.status === 'fulfilled') {
-        return {
-          status: results.status,
-          value: results.values,
-        };
-      }
-      return {
-        status: 'rejected',
-        value: results.reason,
-      };
-    });
+    .then((results) => ({
+      status: results.status,
+      value: results.values,
+    }));
 }
