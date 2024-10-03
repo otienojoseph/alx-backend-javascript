dea0d85e2b4d4e7fab48a8a06cbac13007500db1
otienojoseph <otienoj.cruz@gmail.com>
2024-10-03 06:10:53 +0300

    refactor code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,17 +3,18 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-
-      return {
-        status: result.status,
-        value: result.reason,
-      };
-    }));
+    // .then((results) => results.map((result) => {
+    //   if (result.status === 'fulfilled') {
+    //     return {
+    //       status: result.status,
+    //       value: result.value,
+    //     };
+    //   }
+    //
+    //   return {
+    //     status: result.status,
+    //     value: result.reason,
+    //   };
+    // }));
+    .then((results) => results);
 }
