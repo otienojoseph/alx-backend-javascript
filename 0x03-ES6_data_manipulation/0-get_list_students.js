81eafc253a960c76587003f361d2b615401b364f
otienojoseph <otienoj.cruz@gmail.com>
2024-10-09 01:58:35 +0300

    refactor: Use traditional function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,21 @@
-const getListStudents = () => [{
-  id: 1,
-  firstName: 'Guillaume',
-  location: 'San Fransisco',
-},
-{
-  id: 2,
-  firstName: 'James',
-  location: 'Columbia',
-},
-{
-  id: 5,
-  firstName: 'Serena',
-  location: 'San Fransisco',
-},
-];
+function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Fransisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Fransisco',
+    },
+  ];
+}
 
 export default getListStudents;
