9d2e0a18c50a231d64b4a7a3a9c7f9909f268b09
otienojoseph <otienoj.cruz@gmail.com>
2024-10-09 00:59:41 +0300

    fix: Add default export instead

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,10 +1,13 @@
-import {listOfStudents} from "./9-hoisting.js";
+import listOfStudents,{HolbertonClass,StudentHolberton} from "./9-hoisting.js";
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
     student => student.fullStudentDescription
 );
+
+console.log(listOfStudents[0] instanceof StudentHolberton)
+console.log(listOfStudents[0].holbertonClass instanceof HolbertonClass)
 
 console.log(listPrinted)
 
