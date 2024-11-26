d5cf201ce2bd350a41de2c21df46cdaa5e4c9ade
otienojoseph <otienoj.cruz@gmail.com>
2024-11-26 22:23:27 +0300

    fix: Change parameter name

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,6 +3,8 @@
     * @param (string) message - The string to print 
     * @returns (string) Prints message to stdout
 */
-const displayMessage = (message) => console.log(message);
+const displayMessage = (message) => {
+  console.log(message);
+};
 
 module.exports = displayMessage;
