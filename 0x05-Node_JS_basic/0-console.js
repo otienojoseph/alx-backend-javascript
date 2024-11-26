a89a988bf3a7b3d9f48bf9621c44e7674f2f34b9
otienojoseph <otienoj.cruz@gmail.com>
2024-11-26 22:04:01 +0300

    Add documentation

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+    * Prints message to the stdout
+    * @param (string) param - The string to print 
+    * @returns (string) Prints message to stdout
+*/
+  
 function displayMessage(param) {
     console.log(param);
 }
