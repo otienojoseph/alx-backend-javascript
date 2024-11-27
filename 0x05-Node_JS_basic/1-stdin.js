899083e431ac4533303c6d1002e7bd61556e237a
otienojoseph <otienoj.cruz@gmail.com>
2024-11-27 19:07:23 +0300

    Add process stdin script

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+/**
+ * Program takes input from command line and prints it to stdout
+ * @returns: Prints to stdout the parameter
+ */
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+console.log("Welcome to Holberton School, what is your name?");
+
+r1.on('line', name => {
+    console.log(`Your name is: ${name}`);
+    r1.close();
+});
+
+r1.on('close', () => {
+    console.log("This important software is closing");
+    process.exit(0);
+})
