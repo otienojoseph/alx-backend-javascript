d288cb04d1af1ed00e06eae73438c52c0a350d40
otienojoseph <otienoj.cruz@gmail.com>
2024-11-28 13:04:17 +0300

    refactor: Revert to using readline

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,18 +5,18 @@ const readline = require('readline');
  * @returns: Prints to stdout the parameter
  */
 const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+    input: process.stdin,
+    output: process.stdout,
+});
 
 console.log("Welcome to Holberton School, what is your name?");
 
-r1.on('line', name => {
-    console.log(`Your name is: ${name}`);
+r1.on('line', line => {
+    console.log(`Your name is: ${line}`);
     r1.close();
 });
 
 r1.on('close', () => {
     console.log("This important software is closing");
     process.exit(0);
-})
+});
