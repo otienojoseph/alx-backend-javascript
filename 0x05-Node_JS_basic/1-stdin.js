0634b1e620e52545c7dc124a163b3d3f271de2c7
otienojoseph <otienoj.cruz@gmail.com>
2024-11-29 12:15:00 +0300

    refactor: Use readline.question

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,14 +9,13 @@ const r1 = readline.createInterface({
     output: process.stdout,
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-r1.on('line', line => {
-    console.log(`Your name is: ${line}`);
+r1.question("Welcome to Holberton School, what is your name?\n", name => {
+    console.log(`Your name is: ${name}`);
     r1.close();
-});
+})
 
 r1.on('close', () => {
     console.log("This important software is closing");
     process.exit(0);
+
 });
