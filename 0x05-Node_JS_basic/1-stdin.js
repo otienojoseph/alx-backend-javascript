65ae3322ca0363109af75d76ef16f852c30652b7
otienojoseph <otienoj.cruz@gmail.com>
2024-12-02 14:42:04 +0300

    fix: Add correct statement

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -17,5 +17,5 @@ process.stdin.on('readable', () => {
   }
 });
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
