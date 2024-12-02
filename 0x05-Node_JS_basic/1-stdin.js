2a7b1a53219eb493b951a16735125600610b5dc8
otienojoseph <otienoj.cruz@gmail.com>
2024-12-02 14:23:53 +0300

    fix: Use correct eslint rules

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,15 +7,15 @@
  * @returns: Prints to stdout "Your name is: INPUT"
  */
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
-    let chunk = process.stdin.read();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-    if (chunk) {
-        process.stdout.write(`Your name is: ${chunk}`);
-    }
-})
-process.stdin.on("end", () => {
-    process.stdout.write("This important software is closing\n");
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is closing\n');
 });
