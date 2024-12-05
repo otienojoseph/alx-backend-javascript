57f7e4079ee7179205f3c0f8368657814abff280
otienojoseph <otienoj.cruz@gmail.com>
2024-12-05 13:42:32 +0300

    fix: eslint issues

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -62,7 +62,7 @@ async function countStudents(path) {
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!\n');
+    res.end('Hello ALX!');
   } else if (req.url === '/students') {
     // Handle /students path
     const databasePath = process.argv.length > 2 ? process.argv[2] : '';
